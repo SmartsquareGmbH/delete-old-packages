37ac6494e0eced8c545909838baade62db84a9bf
Ruben Gees <rubengees7@googlemail.com>
2021-03-26 15:03:29 +0100

    Include uncleaned version in semver test

--- test/process.test.js
+++ test/process.test.js
@@ -30,7 +30,7 @@ test("filters based on semver", () => {
     [
       {
         name: "package 1",
-        versions: [{ version: "1.0.0" }, { version: "2.0.1" }, { version: "3.10.2" }],
+        versions: [{ version: "1.0.0" }, { version: "2.0.1" }, { version: "v3.10.2" }],
       },
     ],
     {
@@ -41,7 +41,7 @@ test("filters based on semver", () => {
 
   expect(result.length).toBe(2)
   expect(result[0].version).toBe("1.0.0")
-  expect(result[1].version).toBe("3.10.2")
+  expect(result[1].version).toBe("v3.10.2")
 })
 
 test("filters based on regex", () => {
