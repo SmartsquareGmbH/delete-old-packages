becea094ebba3952d40d01ffffb194ce205def1f
Ruben Gees <rubengees7@gmail.com>
2022-07-09 20:30:22 +0200

    Improve error handling

--- src/query/organization.container.query.strategy.ts
+++ src/query/organization.container.query.strategy.ts
@@ -5,12 +5,7 @@ export default class OrganizationContainerQueryStrategy implements QueryStrategy
   async queryPackages(input: Input): Promise<Package[]> {
     return await Promise.all(
       input.names.map(async (name) => {
-        const response = await getOctokit(input.token).rest.packages.getAllPackageVersionsForPackageOwnedByOrg({
-          package_name: name,
-          package_type: "container",
-          org: input.organization,
-          per_page: 100,
-        })
+        const response = await this.queryPackage(input, name)
 
         const result: Package = {
           name,
@@ -25,5 +20,18 @@ export default class OrganizationContainerQueryStrategy implements QueryStrategy
         return result
       })
     )
+  }
+
+  private async queryPackage(input: Input, name: string) {
+    try {
+      return await getOctokit(input.token).rest.packages.getAllPackageVersionsForPackageOwnedByOrg({
+        package_name: name,
+        package_type: "container",
+        org: input.organization,
+        per_page: 100,
+      })
+    } catch (e) {
+      throw new Error(`Failed to query package ${name}`)
+    }
   }
 }
