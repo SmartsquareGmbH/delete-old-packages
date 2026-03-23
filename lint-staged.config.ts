f45d66818a3cfee716f6d3c79aef8c71fdd32c59
Ruben Gees <rubengees7@gmail.com>
2026-03-23 10:42:02 +0100

    Fix lint-staged issue

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,4 +1,4 @@
 export default {
   "!(*.{js,ts})": "oxfmt --no-error-on-unmatched-pattern",
-  "*.{js,ts}": ["oxfmt", "oxlint --fix", () => "tsgo --noEmit"],
+  "*.{js,ts}": ["oxfmt --no-error-on-unmatched-pattern", "oxlint --fix", () => "tsgo --noEmit"],
 }
