e681047264ea3d31ca01036e46072c6f401f09ff
Ruben Gees <rubengees7@gmail.com>
2026-04-30 15:55:42 +0200

    Improve pre-commit setup

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,4 +1,8 @@
 export default {
   "!(*.{js,ts})": "oxfmt --no-error-on-unmatched-pattern",
-  "*.{js,ts}": ["oxfmt --no-error-on-unmatched-pattern", "oxlint --fix", () => "tsgo --noEmit"],
+  "*.{js,ts}": [
+    "oxfmt --no-error-on-unmatched-pattern",
+    "oxlint --no-error-on-unmatched-pattern --fix",
+    () => "tsgo --noEmit",
+  ],
 }
